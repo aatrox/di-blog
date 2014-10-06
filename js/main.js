91cce1fa7856cde838044ae546dc59c8f055d21d
Di Wu <diwuusa@gmail.com>
2014-10-06 12:01:45 -0500

    web hosting

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
 
 
 })
+
+$(function(){
+  $('.navbar a.dropdown-toggle').on('click', function(e) {
+    if (!$(this).parent().parent().hasClass('nav')) {
+      $(this).parent().siblings().removeClass('open');
+      var item = $(this).parent();
+      var heightParent = item.index()*26;
+      var widthParent = parseInt($(this).parent().parent().css('width')) - 10;
+      $(this).parent().addClass('open');
+      $(this).next().css('top', heightParent + 'px');
+      $(this).next().css('left', widthParent + 'px');
+      return false;
+    }
+  });
+});
