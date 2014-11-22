dda8be265647a04ca3b2cc14777c3d92ed130547
Di Wu <diwuusa@gmail.com>
2014-11-21 18:04:03 -0600

    carousel

--- js/main.js
+++ js/main.js
@@ -14,15 +14,6 @@ $(document).ready(function(){
   	$(this).closest('ul').closest('li').addClass('active');
   })
 
-  // $('#left-sidr ul ul li').on('click', function(){
-  //   $(this).siblings().removeClass('clickactive');
-  //   $(this).addClass('clickactive');
-  //   $(this).closest('ul').closest('li').siblings().removeClass('active');
-  //   $(this).closest('ul').closest('li').siblings().find('ul').find('li').removeClass('clickactive');
-  //   $(this).closest('ul').closest('li').addClass('active');
-  // })
-
-
   $('#midbtndiv').on('click', function(){
   	$('.mid2nd').show();
   	$(this).hide();
@@ -45,6 +36,30 @@ $(document).ready(function(){
   $('#left-menu').sidr({
     name: 'left-sidr'
   });
+
+  $('#right-menu').sidr({
+    name: 'right-sidr',
+    side: 'right'
+  });
+
+  // jcarousel
+  $('.jcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+        var element = $(this),
+            width = element.innerWidth();
+        if (width > 900) {
+            width = width / 3;
+        } else if (width > 600) {
+            width = width / 2;
+        }
+        // This shows 1 item at a time.
+        // Divide `width` to the number of items you want to display,
+        // eg. `width = width / 3` to display 3 items at a time.
+        element.jcarousel('items').css('width', width + 'px');
+    })
+    .jcarousel({
+        // Your configurations options
+    });
 })
 
 
