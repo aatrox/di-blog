b82ec7dadf95b5c23a78a1972a93882b0d3b37b1
Di Wu <diwuusa@gmail.com>
2014-12-05 15:00:35 -0600

    debug

--- js/main.js
+++ js/main.js
@@ -42,24 +42,8 @@ $(document).ready(function(){
     side: 'right'
   });
 
-  // jcarousel
-  $('.jcarousel')
-    .on('jcarousel:create jcarousel:reload', function() {
-        var element = $(this),
-            width = element.innerWidth();
-        if (width > 900) {
-            width = width / 3;
-        } else if (width > 600) {
-            width = width / 2;
-        }
-        // This shows 1 item at a time.
-        // Divide `width` to the number of items you want to display,
-        // eg. `width = width / 3` to display 3 items at a time.
-        element.jcarousel('items').css('width', width + 'px');
-    })
-    .jcarousel({
-        // Your configurations options
-    });
+  
+  
 })
 
 
